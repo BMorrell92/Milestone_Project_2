428bd64df0fbc4294127686e342df58d33d4e059
BMorrell92 <ben_morrell92@outlook.com>
2022-11-09 20:31:44 +0000

    updated so that background GIF resets from rural

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const playpause = document.querySelector('.playpause');
+// const playpause = document.querySelector('.playpause');
 
-playpause.addEventListener('click', () => {
-  playpause.classList.toggle('playing');
-});
-
+// playpause.addEventListener('click', () => {
+//   playpause.classList.toggle('playing');
+// });
 
 let myAudios = document.querySelector(".all-audios");
 let selected = "ocean";
@@ -17,6 +16,7 @@ const RESET_DASH_ARRAY = `-57 ${FULL_DASH_ARRAY}`;
 let choiceBtn = document.querySelectorAll(".time-select");
 let baseTimer = document.querySelector(".base-timer");
 let pauseBtn = document.querySelector(".base-timer .pause-btn");
+let playBtn = document.querySelector(".base-timer .play-btn");
 
 //DOM elements
 let timer = document.querySelector("#base-timer-path-remaining");
@@ -33,9 +33,9 @@ let timerInterval = null;
 // Pause & Replay
 function control() {
   if (myAudio.paused) {
-    myAudio.play();
-    startTimer();
+    start();
     pauseBtn.style.display = "none";
+    playBtn.style.display = "none";
     timeLabel.style.display = "flex";
   } else {
     stop();
@@ -45,29 +45,39 @@ function control() {
   }
 }
 
-//reset the sound and timer
+//reset & update the sound and timer
 function reset() {
   clearInterval(timerInterval);
-  resetVars();
-  timer.setAttribute("stroke-dasharray", RESET_DASH_ARRAY);
-  myAudio.load();
+  resetVars(); // Reset & update the time
+  myAudio.load(); // Reset the audio
+  setAudio();  // Update the audio
+  timer.setAttribute("stroke-dasharray", RESET_DASH_ARRAY); // Reset the timer circle
+  playBtn.style.display = "flex";
+  pauseBtn.style.display = "none";
+  timeLabel.style.display = "none";
 }
 
 // start the sound and timer
 function start() {
-  if (myAudio.paused) {
-    pauseBtn.style.display = "none";
-    timeLabel.style.display = "flex";
-  }
-  reset();
+  setTime();
+  setAudio()
+  myAudio.play();
+  startTimer();
+}
+
+// set time 
+function setTime() {
   TIME_LIMIT = document.querySelector("input[name='time']:checked").value;
   timeLeft = TIME_LIMIT;
   timeLabel.innerHTML = formatTime(timeLeft);
+}
+
+// set audio and background
+function setAudio() {
   selected = document.querySelector("input[name='sound']:checked").value;
   myAudio = myAudios.querySelector(`[data-audio = "${selected}"]`);
+  document.querySelector("body").className = '';
   document.querySelector("body").classList.add(selected);
-  myAudio.play();
-  startTimer();
 }
 
 // stop the timer
@@ -95,3 +105,44 @@ function timerFunction() {
 window.addEventListener("load", () => {
   timeLabel.innerHTML = formatTime(TIME_LIMIT);
 });
+
+//---------------------------------------------
+//HELPER METHODS
+//---------------------------------------------
+
+function timeIsUp() {
+  clearInterval(timerInterval);
+  myAudio.pause();
+  reset();
+}
+
+// Reset the time
+function resetVars() {
+  timePassed = -1;
+  setTime();
+}
+
+//Convert seconds to minutes format
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+// These 2 functions are for updating SVG stroke-dasharray
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  timer.setAttribute("stroke-dasharray", circleDasharray);
+}
